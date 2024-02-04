5324c4b6c54480ab56e489f33f42bc40b9cfea98
AndreyIvanchin <andreyivanchin000@gmail.com>
2024-02-04 12:28:13 +0300

    start

---
+++ src/main/java/com/ivanchin/seetheweather/SeeTheWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.ivanchin.seetheweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeTheWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeeTheWeatherApplication.class, args);
+    }
+
+}
